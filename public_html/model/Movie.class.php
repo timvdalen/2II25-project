383e477496950997e93299033412a1c91c76cd6d
Tim van Dalen <Tim@timvdalen.nl>
2013-03-09 18:10:39 +0100

    Added Movie class

---
+++ public_html/model/Movie.class.php
@@ -0,0 +1,50 @@
+<?php
+	/**
+	 * Represents a movie
+	 */
+	class Movie{
+		/**
+		 * IMDB ID for this movie
+		 * @var string $imdb_id
+		 */
+		public $imdb_id;
+		
+		/**
+		 * DBpedia id for this movie
+		 * @var string $dbpedia_id
+		 */
+		public $dbpedia_id;
+		
+		/**
+		 * Title for this movie
+		 * @var string $title
+		 */
+		public $title;
+		
+		/**
+		 * Poster for this movie
+		 * @var string $poster
+		 */
+		public $poster;
+		
+		/**
+		 * Overview text for this movie
+		 * @var string $overview
+		 */
+		public $overview;
+		
+		/**
+		 * Trailer link for this moview
+		 * @var string $trailer_link
+		 */
+		public $trailer_link;
+		
+		function __construct($imdb_id, $dbpedia_id, $title, $poster, $overview){
+			$this->imdb_id;
+			$this->dbpedia_id;
+			$this->title;
+			$this->poster;
+			$this->overview;
+		}
+	}
+?>
