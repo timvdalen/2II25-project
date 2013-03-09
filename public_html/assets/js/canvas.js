03d96599f769a4078f582e87c871b86b237c484e
Carl van Dueren den Hollander <carl.vddh@gmail.com>
2013-03-10 00:51:55 +0100

    canvas sync

--- public_html/assets/js/canvas.js
+++ public_html/assets/js/canvas.js
@@ -2,13 +2,13 @@ function Node() {
 	
 }
 
-function drawNode(x, y, size, weight) {
+function drawNode(g, x, y, size, weight) {
 	var color1 = '#000000';
 	var color2 = '#AA0000';
 
 	g.beginPath();
 	g.arc(x, y, size, 0, 2 * Math.PI, false);
-	g.fillStyle = weight * color1 + (1-weight) * color2;
+	g.fillStyle = color2;
 	g.fill();
 	g.lineWidth = 2;
 	g.strokeStyle = color1;
@@ -23,5 +23,15 @@ $(function(){
 	var g = canvas.getContext("2d");
 
 	g.font = "30px Arial"
-	g.fillText($("#graph").width(), 10, 10);
+	g.fillText(canvas.width, 10, 40);
+	g.fillText(canvas.height, 100, 40);
+	
+	drawNode(g, 200, 30, 6, 1);
+	drawNode(g, 250, 30, 8, 1);
+	drawNode(g, 300, 30, 10, 1);
+	drawNode(g, 350, 30, 8, 1);
+	drawNode(g, 400, 30, 6, 1);
 });
+
+
+
