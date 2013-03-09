55907ef03a3cdf7631e9621f1e10b5e4ae67d6d7
Tim van Dalen <Tim@timvdalen.nl>
2013-03-10 00:32:53 +0100

    Hide trailer

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -1,9 +1,20 @@
-$(function(){
-	$("#movie-trailer").click(function(){
-		var trailer = $(this);
-		var height = $("#graph").height();
-		$("#graph").hide(200, function(){
-			$("#embed").attr("src", trailer.data("link")).css("height", height + "px").show(200);
-		});
+function show_trailer(e){
+	var trailer_link = e.target;
+	var height = $("#graph").height();
+	$("#graph").hide(200, function(){
+		$("#embed").attr("src", trailer_link.data("link")).css("height", height + "px").show(200);
+		trailer_link.text("Hide trailer").click(hide_trailer);
 	});
+}
+
+function hide_trailer(e){
+	$("#embed").hide(200, function(){
+		$("embed").attr("src", "about:blank");
+		$("graph").show(200);
+		trailer_link.text("Show trailer").click(show_trailer);
+	});
+}
+
+$(function(){
+	$("#movie-trailer").click(show_trailer);
 });
