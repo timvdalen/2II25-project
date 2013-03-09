b149a11721a714b035bfc61f07a28d22c94b0f13
Carl van Dueren den Hollander <carl.vddh@gmail.com>
2013-03-09 20:28:13 +0100

    Home sidebar

--- public_html/view/Home.view.php
+++ public_html/view/Home.view.php
@@ -1,15 +1,28 @@
 <?php
+	include("model/Movie.class.php");
+
 	/*
 	 * Outputs the main page
 	 */
 	class Home extends View{
+		/*
+		 * Selected movie
+		 * @var Movie $selected
+		 */
+		public $selected;
+	
 		/*
 		 * Constructs the Home page
 		 *
 		 * @param array $arguments
 		 *  Contains the arguments for this page
 		 */
-		function __construct($arguments){}
+		function __construct($arguments){
+			$this->selected = new Movie(0, 0, 
+				"The Hangover Part III (2013)", 
+				"http://cdn1.screenrant.com/wp-content/uploads/The-Hangover-3-Poster-Harry-Potter-570x844.jpg", 
+				"This time, there's no wedding. No bachelor party. What could go wrong, right? But when the Wolfpack hits the road, all bets are off.");
+		}
 		
 		/*
 		 * Renders the Home page
@@ -20,8 +33,26 @@
 		public function render(){
 			$content = <<<ENDHTML
 <div class = "row-fluid">
-	<div class = "span8"></div>
-	<div class = "span4"></div>
+	<div class = "span9">
+
+	</div>
+	<div id = "movie-info" class = "span3 well">
+		<h4>
+ENDHTML;
+			$content .= $this->selected->title;
+			$content .= <<<ENDHTML
+		</h4>
+		<div class = "row-fluid">
+			<div class = "span8 offset2">
+				<img src = {$this->selected->poster}></img>
+			</div>
+		</div>
+		<div>
+ENDHTML;
+			$content .= $this->selected->overview;
+			$content .= <<<ENDHTML
+		</div>
+	</div>
 </div>
 ENDHTML;
 
