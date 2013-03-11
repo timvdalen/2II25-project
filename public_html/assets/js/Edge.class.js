04d56dbc127924bf5246edb724d77d338c9cc47a
nickyadvokaat <nicky.advokaat@nubisonline.nl>
2013-03-11 10:27:14 +0100

    Update Edge.class.js

--- public_html/assets/js/Edge.class.js
+++ public_html/assets/js/Edge.class.js
@@ -1,7 +1,6 @@
-function Node(_moviea, _movieb, weight) {
-	this.moviea = _moviea;
-	this.movieb = _movieb;
-	this.size = _size;
+function Edge(_node1, _node2, _weight) {
+	this.node1 = _node1;
+	this.node2 = _node2;
 	this.mouseover = false;
 	this.weight = _weight;
 	
@@ -9,10 +8,25 @@ function Node(_moviea, _movieb, weight) {
 		var color;
 		var size;
 		if (this.mouseover) {
-		
+			color = '#AA0000';
+			size = this.weight + 2;
 		} else {
-		
+			color = '#000000';
+			size = this.weight;
 		}
+		
+		x1 = this.node1.x;
+		y1 = this.node1.y;
+		x2 = this.node2.x;
+		y2 = this.node2.y;
+		
+		g.save();
+		g.moveTo(x1,y1);
+		g.lineTo(x2,y2);
+		g.lineWidth = size;
+		g.strokeStyle = color;
+		g.stroke();		
+		g.restore();
 	}
 	
 	this.inBounds = function(x, y) {
