0850591ef5c4dd9f2405e6927c9f0f969f7b58cd
Tim van Dalen <Tim@timvdalen.nl>
2013-04-06 09:20:45 +0200

    Added trending user to trakt

--- public_html/ajax/importers/trakt.php
+++ public_html/ajax/importers/trakt.php
@@ -1,6 +1,11 @@
 <?php
 
 $user = $_GET['user'];
-print file_get_contents("http://api.trakt.tv/user/library/movies/watched.json/dbbf41d754a864c3dfd6608bf000063e/$user/extended");
+if($user == "trending"){
+	$data = json_decode(file_get_contents("http://api.trakt.tv/movies/trending.json/dbbf41d754a864c3dfd6608bf000063e"));
+	print json_encode(array_slice($data, 0, 5));
+}else{
+	print file_get_contents("http://api.trakt.tv/user/library/movies/watched.json/dbbf41d754a864c3dfd6608bf000063e/$user/extended");
+}
 
 ?>
