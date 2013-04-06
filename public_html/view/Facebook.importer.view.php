94257d1d8275a0ea542cc542be90d0cb262b7e62
Tim van Dalen <Tim@timvdalen.nl>
2013-04-06 09:30:41 +0200

    Added explanation for not implementing Facebook in public repo

--- public_html/view/Facebook.importer.view.php
+++ public_html/view/Facebook.importer.view.php
@@ -20,7 +20,19 @@
 		 * @var string $body
 		 */
 		protected $body = <<<ENDBODY
-	<p>Feestboektijd</p>
+	<p>Due to the nature of this assignment and the restrictions of the Facebook platform, we have decided not to include the Facebook importer.</p>
+	
+	<p>To show that we did work on it, here is the structure of the importer:</p>
+	<ol>
+		<li>Use the Facebook JS SDK to create a pop-up asking for access to the <code>user_likes</code> OAuth scope</li>
+		<li>If granted, send the generated code to our server using AJAX</li>
+		<li>Here, use our application secret to obtain an <code>access_token</code> from the Graph API</li>
+		<li>Download a list of movies from the Graph API <code>/me/movies</code> (using the <code>access_token</code> as authentication)</li>
+		<li>Return this list from the AJAX call</li>
+		<li>Show the list in the importer window</li>
+	</ol>
+	
+	<p>From that point, this importer works in the same way as the Trakt.tv importer does.</p>
 ENDBODY;
 
 		/*
